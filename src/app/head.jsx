164f2e910c98f505585c482be8936b38b1291106
Adaeze Ndupu <andupu4@gmail.com>
2023-04-03 11:46:09 +0100

    Install Next and Tailwind

---
+++ src/app/head.jsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDb</title>
+      <meta name='description' content='IMDb clone using Next and Tailwind' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+    </>
+  );
+}
